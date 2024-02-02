67855f635d85fd42e5e9d9342a067b673f0bbc1a
Diogo <128392992+DiogoMantos@users.noreply.github.com>
2024-02-02 00:45:15 -0400

    Add files via upload

---
+++ activ15.java
@@ -0,0 +1,16 @@
+
+public class activ15 {
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while(i <= 30){
+            if(i % 3 == 0){
+                 System.out.println(i + " Divididos por 3 ");
+            }
+                i++;
+        }
+        
+    }
+    
+}
